e1d224af27000ba7349a1b2c2e65e86af43f02de
Faizan Jan <faizanjan619@gmail.com>
2023-05-30 10:39:49 +0530

    Separate the filtering of countries from JSX in Home component

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,27 +7,36 @@ let Home = ({ setCountry, isDarkMode }) => {
   let [region, setRegion] = useState(null);
   let [searchText, setSearchText] = useState("");
 
-  return (
-    <div className={(isDarkMode? " text-light bg-dark": " text-dark bg-light")}>
-      <SearchAndFilter setRegion={setRegion} setSearchText={setSearchText} isDarkMode={isDarkMode}/>
+  let countriesToDisplay = allCountries.filter(
+    (country) =>
+      country["name"]["common"]
+        .toLowerCase()
+        .startsWith(searchText.toLowerCase()) &&
+      (region === null || country["region"] === region)
+  );
 
-      <div className={"all-countries p-5 d-flex flex-row justify-content-around flex-wrap"+ (isDarkMode? " bg-dark": " bg-light")}>
-        {allCountries
-          .filter(
-            (country) =>
-              country["name"]["common"]
-                .toLowerCase()
-                .startsWith(searchText.toLowerCase()) &&
-              (region === null || country["region"] === region)
-          )
-          .map((country) => (
-            <CountryCard
-              key={country["name"]["official"]}
-              country={country}
-              setCountry={setCountry}
-              isDarkMode={isDarkMode}
-            />
-          ))}
+  return (
+    <div className={isDarkMode ? " text-light bg-dark" : " text-dark bg-light"}>
+      <SearchAndFilter
+        setRegion={setRegion}
+        setSearchText={setSearchText}
+        isDarkMode={isDarkMode}
+      />
+
+      <div
+        className={
+          "all-countries p-5 d-flex flex-row justify-content-around flex-wrap" +
+          (isDarkMode ? " bg-dark" : " bg-light")
+        }
+      >
+        {countriesToDisplay.map((country) => (
+          <CountryCard
+            key={country["name"]["official"]}
+            country={country}
+            setCountry={setCountry}
+            isDarkMode={isDarkMode}
+          />
+        ))}
       </div>
     </div>
   );
